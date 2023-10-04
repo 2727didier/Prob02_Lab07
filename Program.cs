3379c29fb8cc7e1c7ab18314e8c88e5eacdb6201
USUARIO <USUARIO@DESKTOP-Q5ALF4J>
2023-10-03 19:15:00 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob02_Lab07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"Divisor de Omar Orlandini {contador}: {i}");
+                    contador++;
+                }
+            }
+        }
+    }
+
+}
